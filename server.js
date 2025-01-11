8230ddd59de238f868578a0767c758ec2e3d4441
Briam Carlos Gonzalez Del Rosario <63083603+Briamco@users.noreply.github.com>
2025-01-11 14:58:38 +0000

    add: delete product

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const { RetryAgent } = require('undici-types')
 
 const PORT = 3000
 const app = express()
@@ -32,13 +33,25 @@ app.post('/api/products', (req, res) => {
     }
 
     const newProdcut = {
-        id: new Date(),
+        id: products.length + 1,
         title,
         price: `$${price}` || '$0.00',
     }
 
     products.push(newProdcut)
     res.status(201).json(newProdcut)
+})
+
+app.delete('/api/products/:id', (req, res) => {
+    const { id } = req.body
+    const prodIndex = products.findIndex(prod => prod.id === id)
+
+    if (prodIndex === -1) {
+        return res.status(400).json({ error: "Este producto no existe" })
+    }
+
+    products.slice(prodIndex, 1)
+    res.status(200).json({ message: `El produncto con la id: ${id} fue eliminado` })
 })
 
 app.listen(PORT, () => {
